87df8647eacccde31190b34ff93d5d74aba9e59d
m-be-k <me.madik@mail.ru>
2026-05-18 19:29:16 +0500

    Create Pinia

--- messenger/src/main.js
+++ messenger/src/main.js
@@ -3,8 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from "@/router/router.js";
+import {createPinia} from "pinia";
 
 
 createApp(App)
+    .use(createPinia)
     .use(router)
     .mount('#app')
